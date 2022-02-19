118efff3afb56ea7fa3a424842e0eec5cd936232
js10222 <85411248+js10222@users.noreply.github.com>
2022-02-19 08:49:35 -0800

    Create p.js

---
+++ p.js
@@ -0,0 +1,12 @@
+VANTA.RINGS({
+  el: "#ecosystem-v",
+  mouseControls: true,
+  touchControls: true,
+  gyroControls: false,
+  minHeight: 200.00,
+  minWidth: 200.00,
+  scale: 1.00,
+  scaleMobile: 1.00,
+  backgroundColor: 0x011119,
+	color: 0x00a6fb
+})
